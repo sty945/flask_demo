3952df8876604953508527797c3e1ee41d025240
sty945 <sutaoyu@yeah.net>
2019-07-26 10:38:40 +0800

    增加————加__init__.py文件

---
+++ flaskr/__init__.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+# !/usr/bin/env python
+
+# Time: 2019/7/26 10:21
+
+# Author: sty
+
+# File: __init__.py.py
+
+import  os
+
+from flask import Flask
+
+def create_app(test_config=True):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # a simple page that says hello
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+
+    return app
+
+
